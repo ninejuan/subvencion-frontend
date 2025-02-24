c11bbbe597fc0c3fec0ad38797c0a0a944c71b90
Juan Lee <ninejuan@protonmail.com>
2025-02-24 23:55:57 +0900

    build: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
+  server: {
+    cors: {
+      origin: [
+        "https://subvencion.juany.kr",
+        "https://api-subvencion.juany.kr",
+      ],
+      methods: ["POST", "GET", "PATCH", "PUT", "OPTIONS", "DELETE"],
+      allowedHeaders: ["*"],
+    },
+    allowedHosts: [
+      "https://subvencion.juany.kr",
+      "https://api-subvencion.juany.kr",
+    ],
+  },
+});
